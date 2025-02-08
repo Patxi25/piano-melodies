208a90eb017949fe54b1eb6eac2558a6cb4685a7
Patxi25 <francisco_alberdi@bellsouth.net>
2025-02-07 23:23:29 -0500

    Adding the review component

---
+++ src/app/components/review/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./Review.module.css";
+
+interface ReviewProps {
+  imageSrc: string;
+  imageAlt: string;
+  reviewText: string;
+  reviewerName: string;
+}
+
+const Review: React.FC<ReviewProps> = ({
+  imageSrc,
+  imageAlt,
+  reviewText,
+  reviewerName,
+}) => (
+  <div className={styles.review}>
+    <div className={styles.reviewImageContainer}>
+      <Image
+        src={imageSrc}
+        alt={imageAlt}
+        width={150}
+        height={150}
+        className={styles.reviewImage}
+      />
+    </div>
+    <div className={styles.reviewContent}>
+      <p>{reviewText}</p>
+      <footer>- {reviewerName}</footer>
+    </div>
+  </div>
+);
+
+export default Review;
