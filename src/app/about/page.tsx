6bb666eb7a8231b7aa308377cb06a5b1faef1798
Patxi25 <francisco_alberdi@bellsouth.net>
2025-06-24 18:21:01 -0400

    Removing Valeria

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -44,20 +44,6 @@ const AboutPage = () => (
         imageAlt="Nicole Portrait"
         imagePosition="right"
       />
-
-      {/* Employee 2 Section */}
-      <ContentSection
-        title="Meet Valeria"
-        description={[
-          "Valeria Peñaranda is a versatile Bolivian artist based in Miami, FL. As a singer-songwriter, pianist, producer, arranger, and sound engineer, she has captivated audiences on international stages such as the Suena Caracas Latin American Music Festival, where she shared the spotlight with icons like Café Tacuba and Cultura Profética, the International Folk Festival, and the Latin American VIVE Music Festival at the prestigious Berklee Performance Center. In Bolivia, she has left her mark at Festijazz and Festiblues, and recently showcased her talent alongside Argentine legend Leo Dan at Miami’s renowned James Knight Center.",
-          "She is a recipient of scholarships from Berklee College of Music and the Frost School of Music, Valeria combines academic excellence with artistic brilliance. In 2024, she was recognized as one of 10 women selected for the Latin GRAMMY Leading Ladies of Entertainment Mentorship Program, where she received guidance from influential leaders in the Latin music industry.",
-          "Since 2017, Valeria has also been a dedicated educator, teaching voice and piano to students of all ages. She specializes in contemporary vocal styles like rock, pop, soul, musical theater, and Latin, while her piano instruction follows a classical approach rooted in conservatory standards.",
-          "She further honed her teaching expertise as a Teaching Assistant at the Frost School of Music, focusing on Music Industry and Media Scoring & Production. With years of performance, production, and teaching expertise, she brings a dynamic and well-rounded approach to helping students excel in their musical journey.",
-        ]}
-        imageSrc="/images/portraits/valeria_portrait.png"
-        imageAlt="Valeria Portrait"
-        imagePosition="left"
-      />
     </main>
   </div>
 );
